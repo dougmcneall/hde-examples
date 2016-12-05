e4a816986ac7e2840298ba3b6f5f9cc3b9bef8a9
dougmcneall <dougmcneall@gmail.com>
2016-12-05 15:40:48 +0000

    updating

--- hde-direct-vs-pc.R
+++ hde-direct-vs-pc.R
@@ -19,3 +19,17 @@ yg <- brewer.pal(9, "YlGn")
 X = full_frac[,2:8]
 X.norm = normalize(X)
 X.stan.norm = normalize(matrix(X.standard, nrow = 1), wrt=X)
+
+
+# LOOCV reconstruct using dimension reduction
+bl.frac.ens.pc.cv = matrix(NA, nrow=nrow(bl.frac.ens), ncol = ncol(bl.frac.ens))
+for(i in 1:nrow(X)){
+  X.trunc = X.norm[-i, ]
+  Y.trunc = bl.frac.ens[-i, ]
+  X.target = X.norm[i, ]
+  Y.target = bl.frac.ens[i, ]
+  Y.recon = kmpar.pc(Y=Y.trunc, X=X.trunc, newdata=X.target, num.pc=6)
+  bl.frac.ens.pc.cv[i, ] = Y.recon$tens
+}
+
+# LOOCV reconstruct on a per-gridbox basis
