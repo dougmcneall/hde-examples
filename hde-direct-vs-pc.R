b956edbba630bca4dd98adc1c0f62c2856e2a5ae
dougmcneall <dougmcneall@gmail.com>
2016-12-06 16:27:48 +0000

    Have added a wrapper function to predict.km (in direct.pred) that appears to work but needs testing.

--- hde-direct-vs-pc.R
+++ hde-direct-vs-pc.R
@@ -21,14 +21,6 @@ X.norm = normalize(X)
 X.stan.norm = normalize(matrix(X.standard, nrow = 1), wrt=X)
 
 
-# LOOCV reconstruction by direct emulation
-
-# Looking at a 24 hour compute time for direct prediction on the mac
-# Not working with NAs very well at the moment
-ptm = proc.time()
-test = direct.pred(form = ~., X = X.norm, Y = bl.frac.ens[ , 512:520], Xnew=X.stan.norm)
-direct.time = proc.time() - ptm
-
 # LOOCV reconstruct using dimension reduction
 bl.frac.ens.pc.cv = matrix(NA, nrow=nrow(bl.frac.ens), ncol = ncol(bl.frac.ens))
 for(i in 1:nrow(X)){
@@ -41,3 +33,64 @@ for(i in 1:nrow(X)){
 }
 
 # LOOCV reconstruct on a per-gridbox basis
+
+km.wrap2 = function (form, em, ...){
+  out = NA
+  fit = try(DiceKriging::km(form, design = em$X, response = em$y, 
+                            control = list(trace = FALSE), ...), silent = TRUE)
+  out = fit
+  out
+}
+
+km.pred.wrap = function(kmobj, Xnew, ...){
+  
+  pred = try(DiceKriging::predict.km(kmobj, newdata=Xnew, ...))
+  pred
+             
+}
+
+extract.predmean = function(predobj){
+  if (class(predobj) == "try-error"){
+    out=NA
+  } 
+  else {
+    out = predobj$mean
+  }
+  out
+}
+
+extract.predsd = function(predobj){
+  if (class(predobj) == "try-error"){
+    out = NA
+  } 
+  else {
+    out = predobj$sd
+  }
+  out
+}
+
+direct.pred2 = function (form, X, Y, Xnew, ...){
+  ens.list = emlist(X = X, Y = Y)
+  km.list = mclapply(ens.list, FUN = km.wrap2, form = form)
+  
+  # function fails here - a single NA at the start produces
+  # alternating NAs
+  #pred.list = mclapply(km.list, FUN = predict, newdata = as.matrix(Xnew, nrow = 1), type = "UK")
+  pred.list = mclapply(km.list, FUN = km.pred.wrap, Xnew = as.matrix(Xnew, nrow = 1), type = "UK")
+  
+  out.mean = sapply(pred.list, FUN=extract.predmean)
+  # out.mean = sapply(pred.list, function(x) x$mean)
+  out.sd = sapply(pred.list, FUN=extract.predsd)
+  #out.sd = sapply(pred.list, function(x) x$sd)
+  return(list(mean = out.mean, sd = out.sd))
+}
+
+# LOOCV reconstruction by direct emulation
+# Looking at a 24 hour compute time for direct prediction on the mac
+# direct.pred() isn't working with NAs, and is crapping out the whole mclapply call.
+#ptm = proc.time()
+
+test = direct.pred2(form = ~., X = X.norm, 
+                    Y = bl.frac.ens[ , 511:514], Xnew=X.stan.norm)
+
+#direct.time = proc.time() - ptm
